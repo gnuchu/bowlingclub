69cbfe9afd0d3e891a9086cfecb17b9e1fad387a
root <root@miltonofcampsiebowlingclub.co.uk>
2016-09-14 23:38:00 +0000

    edits

--- theme/summit/footer.inc.php
+++ theme/summit/footer.inc.php
@@ -12,6 +12,6 @@
 <?php get_footer(); ?>
 
 <div id="footer">
-	<p>&copy; <?php echo date('Y'); ?> <a href="<?php get_site_url(); ?>" ><?php get_site_name(); ?></a>  | Summit Theme from <a href="http://osdesk.org/" target="_blank">osDesk</a> based on <a href="http://www.freecsstemplates.org" target="_blank">FCT</a>.  <?php get_site_credits(); ?></p>
+	<p>&copy; <?php echo date('Y'); ?> <a href="<?php get_site_url(); ?>" ><?php get_site_name(); ?></a> </p>
 </div>
 <!-- end #footer -->
